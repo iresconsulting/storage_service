561eec809b12afecba94015560708cd06aa1394c
irene <keiko103@bu.edu>
2022-01-19 18:19:35 +0800

    [global] add utils

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,21 +1,23 @@
 import moment from 'moment'
 
-export function validateDate(date: string){
+export function isValidDate(date: string) {
   const _isValid = moment(date).isValid()
   if (!_isValid) {
     throw new Error('invalid date')
   }
 }
 
-export function validateDateRange({
+export function isValidDateRange({
   startDate,
   endDate
 }: { startDate: string, endDate: string }) {
   return moment(startDate) <= moment(endDate)
 }
 
-export function serializeDateToStartOfDay(str: string) {
-  if (str === 'ongoing') {
+const ONGOING_FLAG = 'ongoing'
+
+export function getSerializedDateStartOfDay(str: string) {
+  if (str === ONGOING_FLAG) {
     return moment().utc().add(100, 'days').startOf('day').toISOString()
   } else if (str === '' || str === 'start') {
     return moment('1976-01-01').startOf('day').utc().toISOString()
@@ -23,8 +25,8 @@ export function serializeDateToStartOfDay(str: string) {
   return moment(str).startOf('day').utc().toISOString()
 }
 
-export function serializeDate(str: string) {
-  if (str === 'ongoing') {
+export function getSerializedDate(str: string) {
+  if (str === ONGOING_FLAG) {
     return moment().utc().add(100, 'days').toISOString()
   } else if (str === '' || str === 'start') {
     return moment('1976-01-01').utc().toISOString()
