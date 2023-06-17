e2009c4fe2a243f83ef39522b092a883bbad6cd0
Irene <irene@Irenes-Mac-mini.local>
2023-06-17 18:42:32 +0800

    add folders api

--- src/tasks/clearPublic.js
+++ src/tasks/clearPublic.js
@@ -2,7 +2,16 @@ import fs from "node:fs/promises";
 import Logger from "../utils/logger";
 
 export default async function clearPublicDir() {
-  const dir = `${process.cwd()}/src/public/`
+  let dir = `${process.cwd()}/src/public/`
+  try {
+    for (const file of await fs.readdir(dir)) {
+      await fs.unlink(`${dir}${file}`);
+    }
+  } catch (e) {
+    Logger.generateTimeLog({ label: Logger.Labels.JOB, message: String(e) })
+  }
+
+ dir = `${process.cwd()}/src/uploads/`
   try {
     for (const file of await fs.readdir(dir)) {
       await fs.unlink(`${dir}${file}`);
