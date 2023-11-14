153f488ded6c164aa3004ed0414a9edeca38ef4f
unknown <you@example.com>
2023-11-14 12:13:04 +0800

    updare config

--- src/index.ts
+++ src/index.ts
@@ -18,7 +18,7 @@ const app: Application = express()
 app.use(cors())
 app.use(express.json({ limit: '500mb' }))
 app.use(express.urlencoded({ extended: false, limit: '500mb' }))
-app.use(express.limit('10M'))
+app.use(express.limit('10mb'))
 app.use(cookieParser())
 
 app.use('/', indexRouter)
